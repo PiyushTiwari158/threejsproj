fc420a0d2ac1df34dde2cc5865d4c2e7949573e5
PiyushTiwari158 <69616378+PiyushTiwari158@users.noreply.github.com>
2023-05-24 18:02:56 +0530

    add github pages

---
+++ src/components/Effects.jsx
@@ -0,0 +1,36 @@
+import { 
+    EffectComposer,
+    DepthOfField
+} from 'react-postprocessing'
+import { useState, useEffect } from 'react'
+import { useThree } from 'react-three-fiber'
+
+const Effects = () => {
+    const [lights, setLights] = useState(null)
+    const { scene } = useThree()
+    useEffect(() => {
+        if (scene.lights && scene.lights.length === 3)
+            setLights(scene.lights)
+    }, [scene.lights])
+    return (
+        lights ? 
+        <EffectComposer>
+            <DepthOfField
+                focusDistance={0}
+                focalLength={0.02}
+                bokehScale={2}
+                height={480}
+            />
+            {/* {lights.map(light => 
+                <GodRays 
+                    key={light.current.uuid} 
+                    sun={light.current}
+                />
+            )} */}
+        </EffectComposer>
+        :
+        null
+    )
+}
+
+export default Effects
